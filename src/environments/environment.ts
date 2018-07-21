215c1fac8e09555b9433fe30bca2c8e6dbddf8ff
hendraw <mailhendra2@gmail.com>
2018-07-21 16:23:31 +0700

    add initiale

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+      apiKey: "AIzaSyAnjl6GZLY-lbonFBpJvMd3gq-tonqSChc",
+      authDomain: "uasapps-1567a.firebaseapp.com",
+      databaseURL: "https://uasapps-1567a.firebaseio.com",
+      projectId: "uasapps-1567a",
+      storageBucket: "",
+      messagingSenderId: "364580257350"
+  }
+};
+
+
+// var config = {
+//   apiKey: "AIzaSyAnjl6GZLY-lbonFBpJvMd3gq-tonqSChc",
+//   authDomain: "uasapps-1567a.firebaseapp.com",
+//   databaseURL: "https://uasapps-1567a.firebaseio.com",
+//   projectId: "uasapps-1567a",
+//   storageBucket: "",
+//   messagingSenderId: "364580257350"
+// };
